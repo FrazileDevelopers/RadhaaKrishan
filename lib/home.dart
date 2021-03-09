02f680d14c6d7487a92dfed72bc5feb6e30c69c3
FrazileDevelopers <frazile.com@gmail.com>
2021-03-10 00:17:12 +0530

    Gird view added

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'imagetile.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'imagelist.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,11 +12,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Image.network(
-          'https://frazilemedia.com/bgpapers/RADHAKRISHAN/RK_100.png',
-          fit: BoxFit.cover,
+      appBar: AppBar(
+        title: Center(
+          child: Text('Staggered Grid'),
+        ),
+      ),
+      body: Container(
+        child: StaggeredGridView.countBuilder(
+          shrinkWrap: true,
+          primary: false,
+          padding: const EdgeInsets.all(12.0),
+          crossAxisCount: 4,
+          mainAxisSpacing: 24,
+          crossAxisSpacing: 12,
+          itemCount: images.length,
+          itemBuilder: (BuildContext context, int index) => ImageTile(
+            image: images[index],
+          ),
+          staggeredTileBuilder: (int index) => StaggeredTile.fit(2),
         ),
       ),
     );
